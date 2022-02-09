a4addfdf67fc8ec9ecd8aee6251c637d14394684
projetodevnx <gabrielcrs2016@gmail.com>
2022-02-09 09:59:27 -0300

    up

---
+++ components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'reactstrap';
+
+export function Header () {
+    return (
+        <div>
+
+        </div>
+    )
+}

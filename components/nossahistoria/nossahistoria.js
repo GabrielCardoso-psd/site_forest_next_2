119a11b3cabdbdf5107ffc470f11f0dccd8b3541
projetodevnx <gabrielcrs2016@gmail.com>
2022-02-09 17:58:01 -0300

    up

--- components/nossahistoria/nossahistoria.js
+++ components/nossahistoria/nossahistoria.js
@@ -8,7 +8,29 @@ export function Nossahistoria () {
     return (
         <div>
             <div className={css.bgsessao}>
+                <Container fluid>
+                    <Row>
+                        <Col>
+                            <div>MÁQUINA</div>
+                        </Col>
 
+                        <Col>
+                            <div className={css.conheca}>Conheça sobre</div>
+                            <div className={css.historia}>
+                                Nossa história
+                                <div className={css.historia2}>
+                                    NOSSA HISTÓRIA
+                                </div>
+                            </div>
+                            <div>Oferecemos uma linha completa de equipamentos florestais para atender às necessidades de sua aplicação
+                                – das principais máquinas que auxiliam na construção de estradas, em serrarias e reflorestamento, até
+                                máquinas criadas para fins específicos, que colhem, extraem e carregam, a linha de produtos da PESA®
+                                Forest é tudo o que você precisa para terminar o trabalho.
+                            </div>
+                            <button>SABER MAIS</button>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </div>
     )

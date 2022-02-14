f99104f73ef25b5591bd545a18369c78bc331dd6
Gabriel Cardoso <gabrielcrs2016@gmail.com>
2022-02-14 12:07:33 -0300

    up

--- components/operacao/operacao.js
+++ components/operacao/operacao.js
@@ -10,7 +10,7 @@ export function Operacao () {
         <Container fluid>
         <Row className={css.row}>
             <Col className={css.column-1}>
-                <h6>O que você precisa para a sua</h6>
+                <h6 className={css.frase}>O que você precisa para a sua</h6>
                 <h1 className={css.operacao}>Operação</h1>
                 <div>
                     A PTH-18 supera os competidores da sua classe de tamanho em capacidade, devido sua lança e
